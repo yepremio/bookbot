f8df13ba419d44261c1e3c2a0b09c84edb52bf25
yepremio <yeprem86@gmail.com>
2024-06-13 10:59:29 -0700

    Added function to produce report

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_of_words = word_count(text)
+    print(f"--- Begin report of books/frankenstein.txt ---")
     print(f"{num_of_words} of words found in the document")
     char_count = count_char(text) 
-    print(char_count)
+    pop_report = report(char_count)
+
+def report(char_count):
+    for char in char_count:
+        print(f"The {char} character was found {char_count[char]} times")
+
 
 def count_char(text):
     char_found = {}
