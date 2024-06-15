fdc812ac2fc23c9253360e1a097ec69511f1f5b7
yepremio <yeprem86@gmail.com>
2024-06-14 20:45:40 -0700

    Added sort functioni

--- main.py
+++ main.py
@@ -2,25 +2,44 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_of_words = word_count(text)
-    print(f"--- Begin report of books/frankenstein.txt ---")
-    print(f"{num_of_words} of words found in the document")
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_of_words} words found in the document")
     char_count = count_char(text) 
-    pop_report = report(char_count)
+    sorted_list = report(char_count)
+    
+    print()
 
-def report(char_count):
-    for char in char_count:
-        print(f"The {char} character was found {char_count[char]} times")
+    for item in sorted_list:
+        if item["char"].isalpha():
+            print(f"The {item['char']} character was found {item['num']} times")
 
+    print(f"--- End report ---")
+
+
+def sort_on(item):
+    return item["num"]
+
+def report(count):
+    new_char_list = []
+    
+    # Convert the dict into a list of dicts
+
+    for char in count:
+            new_char_list.append({"char": char, "num": count[char]})
+
+    new_char_list.sort(reverse=True, key=sort_on)
+
+    return new_char_list
 
 def count_char(text):
     char_found = {}
     lower_case_text = text.lower()
-    
-    for char in lower_case_text:
-        char_found[char] = 0
+
     for chars in lower_case_text:
         if chars in char_found:
             char_found[chars] += 1
+        else:
+            char_found[chars] = 1
 
     return char_found
 
